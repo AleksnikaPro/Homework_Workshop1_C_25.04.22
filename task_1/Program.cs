a5ee47aea3641e806e0ebde61c3f6de28feda76b
Aleksandra Popova <alexnika@bk.ru>
2022-04-28 13:14:39 +0300

    added task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int a =  int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b =  int.Parse(Console.ReadLine());
+
+if (b == a * a)
+{
+    Console.Write("Первое число является квадратом второго числа ");
+}
+else
+{
+   Console.Write("Первое число не является квадратом второго числа ");
+} 
+
+
