bd2ddafe558d1a7fddad97fc827af6e4a10d824f
Aleksandra Popova <alexnika@bk.ru>
2022-04-28 13:17:02 +0300

    added task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int n = int.Parse(Console.ReadLine());
+
+if(n == 1)
+{
+Console.WriteLine("понедельник ");
+}
+if(n == 2)
+{
+Console.WriteLine("вторник ");
+}
+if(n == 3)
+{
+Console.WriteLine("среда ");
+}
+if(n == 4)
+{
+Console.WriteLine("четверг ");
+}
+if(n == 5)
+{
+Console.WriteLine("пятница ");
+}
+if(n == 6)
+{
+Console.WriteLine("суббота ");
+}
+if(n == 7)
+{
+Console.WriteLine("воскресенье ");
+}
