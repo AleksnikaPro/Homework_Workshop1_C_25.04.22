8a1f235aae897cf0fcd545158553af7adc5b92ff
Aleksandra Popova <alexnika@bk.ru>
2022-04-28 13:27:36 +0300

    added task_5

---
+++ task_5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int number =  int.Parse(Console.ReadLine());
+int result = number / 10;
+if (number > 99 && number < 1000)
+{
+Console.Write("Вторая цифра трёхзначного числа: ");
+Console.Write(result = result % 10);
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное!");
+}
+
