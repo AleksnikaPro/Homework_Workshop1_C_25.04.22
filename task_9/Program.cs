52b8280b5e001ed243079b95cd71ba03c560151c
Aleksandra Popova <alexnika@bk.ru>
2022-04-28 13:34:19 +0300

    added task_9

---
+++ task_9/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int result = number / 100;
+
+if (number > 99)
+{
+   Console.Write("Третья цифра введенного числа: ");
+   Console.Write(result = result % 10);
+}
+else
+{
+   Console.WriteLine("Число содержит менее трёх цифр, поэтому найти третью цифру числа невозможно!");
+}
+   
