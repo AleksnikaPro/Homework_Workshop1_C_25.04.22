3eecfd3841181bd33f2be94c189d58f7abaecb5c
Aleksandra Popova <alexnika@bk.ru>
2022-04-28 15:19:43 +0300

    added task_8

---
+++ task_8/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите делимое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите делитель числа: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA % numberB == 0)
+{
+    Console.WriteLine("Число кратно заданному числу! ");
+}
+if (numberA % numberB != 0)
+{
+    Console.WriteLine("Число не кратно заданному числу! Остаток от деления: ");
+    Console.WriteLine(numberA % numberB);
+}
+    
+
+
+
+
