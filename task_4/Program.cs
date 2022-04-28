157a1d6c45e181017ca090e37fc852049f7ed5ad
Aleksandra Popova <alexnika@bk.ru>
2022-04-28 16:26:18 +0300

    added task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int number =  int.Parse(Console.ReadLine());
+int digit = 1;
+
+while(digit <= number)
+{
+    if (digit % 2 == 0)
+    {
+      Console.Write("Четные числа от 1 до указанного Вами числа: ");
+      Console.WriteLine(digit);
+      
+    }
+    digit++;
+}
+
