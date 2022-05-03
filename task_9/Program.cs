0674fce7caf1950a2d8b40a699d2e6150410d700
Aleksandra Popova <alexnika@bk.ru>
2022-05-03 17:20:16 +0300

    added changes in task_9

--- task_9/Program.cs
+++ task_9/Program.cs
@@ -1,16 +1,21 @@
 ﻿// Найти третью цифру числа или сообщить, что её нет.
 
-Console.Write("Введите число: ");
+Console.Write("Введите число до 10000: ");
 int number = int.Parse(Console.ReadLine());
-int result = number / 100;
+int result = number;
 
-if (number > 99)
+if (number > 99 && number < 999)
 {
    Console.Write("Третья цифра введенного числа: ");
-   Console.Write(result = result % 10);
+   Console.Write(result % 10);
+}
+else if (number > 1000 && number < 10000)
+{
+   Console.Write("Третья цифра введенного числа: ");
+   Console.Write(result / 10 % 10);
 }
 else
 {
    Console.WriteLine("Число содержит менее трёх цифр, поэтому найти третью цифру числа невозможно!");
 }
-   
+
